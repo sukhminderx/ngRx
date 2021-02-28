66bce3bcbbf6806dcecc1e05bc93f38490291dd2
Sukhi <sukhmindersinghparmarsocialnet@gmail.com>
2021-02-28 13:21:36 +0530

    with selectors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
-import { MyCounterComponent } from './my-counter/my-counter.component';
-
-
-import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './store/reducers/counter.reducer';
-
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MyCounterComponent,
-  ],
   imports: [
-    BrowserModule, StoreModule.forRoot({ count: counterReducer })
+    BrowserModule,
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, BookListComponent, BookCollectionComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
